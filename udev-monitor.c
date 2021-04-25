13bbe6c1d79d522682f81bd31a4fe86f0a62a8a5
Vladimir Kondratyev <vladimir@kondratyev.su>
2021-04-25 13:04:51 +0300

    Replace kqueue with poll in monitoring event loop.
    To allow it be compiled under Linux.

--- udev-monitor.c
+++ udev-monitor.c
@@ -34,11 +34,11 @@
 #include "utils.h"
 
 #include <sys/types.h>
-#include <sys/event.h>
 #include <sys/queue.h>
 
 #include <errno.h>
 #include <fcntl.h>
+#include <poll.h>
 #include <pthread.h>
 #include <signal.h>
 #include <stdatomic.h>
@@ -64,7 +64,6 @@ struct udev_monitor_queue_entry {
 struct udev_monitor {
 	_Atomic(int) refcount;
 	int fds[2];
-	int kq;
 	struct udev_filter_head filters;
 	struct udev *udev;
 	struct udev_monitor_queue_head queue;
@@ -185,81 +184,65 @@ parse_devd_message(char *msg, char *syspath, size_t syspathlen)
 	return (action);
 }
 
-/* Opens devd socket and set read kevent on success or timer kevent on failure */
-static int
-devd_connect(int kq)
-{
-	int devd_fd;
-	struct kevent ke;
-
-	devd_fd = socket_connect(DEVD_SOCK_PATH);
-
-	if (devd_fd >= 0) {
-		EV_SET(&ke, devd_fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, 0);
-		if (kevent(kq, &ke, 1, NULL, 0, NULL) < 0) {
-			close(devd_fd);
-			devd_fd = -1;
-		}
-	}
-
-	/* Set respawn timer */
-	if (devd_fd < 0) {
-		EV_SET(&ke, 1, EVFILT_TIMER, EV_ADD | EV_ENABLE | EV_ONESHOT,
-		    0, DEVD_RECONNECT_INTERVAL, 0);
-		if (kevent(kq, &ke, 1, NULL, 0, NULL) < 0)
-			devd_fd = -1;
-	}
-
-	return (devd_fd);
-}
-
 static void *
 udev_monitor_thread(void *args)
 {
 	struct udev_monitor *um = args;
 	char ev[1024], syspath[DEV_PATH_MAX];
-	int devd_fd = -1, ret, action;
-	struct kevent ke;
+	struct pollfd fds[2];
+	nfds_t nfds;
+	int devd_fd = -1, ret, action, timeout;
 	sigset_t set;
 
 	sigfillset(&set);
 	pthread_sigmask(SIG_BLOCK, &set, NULL);
 
+	fds[0].fd = um->fds[1];
+	fds[0].events = 0;
+	fds[1].events = POLLIN;
+
 	for (;;) {
 		if (devd_fd < 0)
-			devd_fd = devd_connect(um->kq);
+			devd_fd = socket_connect(DEVD_SOCK_PATH);
 
-		ret = kevent(um->kq, NULL, 0, &ke, 1, NULL);
+		if (devd_fd < 0) {
+			nfds = 1;
+			timeout = DEVD_RECONNECT_INTERVAL;
+		} else {
+			fds[1].fd = devd_fd;
+			nfds = 2;
+			timeout = -1;
+		}
+
+		ret = poll(fds, nfds, timeout);
 		if (ret == -1 && errno == EINTR)
 			continue;
-		if (ret < 1)
+		if (ret == -1)
 			break;
 
 		/* edev_monitor is finishing */
-		if (ke.filter == EVFILT_USER)
+		if (fds[0].revents & POLLHUP)
 			break;
 
 		/* connection respawn timer expired */
-		if (ke.filter == EVFILT_TIMER) {
+		if (ret == 0 || devd_fd < 1)
 			continue;
+
+		if (fds[1].revents & POLLIN) {
+			if (socket_readline(devd_fd, ev, sizeof(ev)) < 0) {
+				close(devd_fd);
+				devd_fd = -1;
+				continue;
+			}
+			action = parse_devd_message(ev, syspath, sizeof(syspath));
+			if (action != UD_ACTION_NONE &&
+			    udev_filter_match(um->udev, &um->filters, syspath))
+				udev_monitor_send_device(um, syspath, action);
 		}
 
-		/* XXX: assert() should be placed here */
-		if (ke.filter != EVFILT_READ)
-			continue;
-
-		if (ke.flags & EV_EOF ||
-		    socket_readline(devd_fd, ev, sizeof(ev)) < 0) {
+		if (fds[1].revents & POLLHUP) {
 			close(devd_fd);
 			devd_fd = -1;
-			continue;
-		}
-
-		action = parse_devd_message(ev, syspath, sizeof(syspath));
-
-		if (action != UD_ACTION_NONE) {
-			if (udev_filter_match(um->udev, &um->filters, syspath))
-				udev_monitor_send_device(um, syspath, action);
 		}
 	}
 
@@ -287,7 +270,6 @@ udev_monitor_new_from_netlink(struct udev *udev, const char *name)
 
 	um->udev = udev;
 	_udev_ref(udev);
-	um->kq = -1;
 	atomic_init(&um->refcount, 1);
 	udev_filter_init(&um->filters);
 	STAILQ_INIT(&um->queue);
@@ -311,28 +293,13 @@ udev_monitor_enable_receiving(struct udev_monitor *um)
 {
 
 	TRC("(%p)", um);
-	struct kevent ev;
-
-	um->kq = kqueue();
-	if (um->kq < 0)
-		goto error;
-
-	EV_SET(&ev, 1, EVFILT_USER, EV_ADD | EV_ENABLE | EV_CLEAR, 0, 0, 0);
-	if (kevent(um->kq, &ev, 1, NULL, 0, NULL) < 0)
-		goto error;
 
 	if (pthread_create(&um->thread, NULL, udev_monitor_thread, um) != 0) {
 		ERR("thread_create failed");
-		goto error;
+		return (-1);
 	}
 
 	return (0);
-error:
-	if (um->kq >= 0) {
-		close (um->kq);
-		um->kq = -1;
-	}
-	return (-1);
 }
 
 LIBUDEV_EXPORT int
@@ -368,16 +335,10 @@ udev_monitor_queue_drop(struct udev_monitor_queue_head *umqh)
 LIBUDEV_EXPORT void
 udev_monitor_unref(struct udev_monitor *um)
 {
-	struct kevent ev;
-
 	TRC("(%p) refcount=%d", um, um->refcount);
 	if (atomic_fetch_sub(&um->refcount, 1) == 1) {
-		EV_SET(&ev, 1, EVFILT_USER, 0, NOTE_TRIGGER, 0, 0);
-		kevent(um->kq, &ev, 1, NULL, 0, NULL);
-		pthread_join(um->thread, NULL);
-		close(um->kq);
-
 		close(um->fds[0]);
+		pthread_join(um->thread, NULL);
 		close(um->fds[1]);
 		udev_filter_free(&um->filters);
 		udev_monitor_queue_drop(&um->queue);
