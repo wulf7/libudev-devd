wulf7/libudev-devd

commit b2c96ce66a129c70d26ea1aeabb9ed7fddc24fe7

Use SEQPACKET socket instead of STREAM one to communicate with devd

udev-monitor.c
@@ -28,6 +28,7 @@
 
 #include <sys/types.h>
 #include <sys/queue.h>
+#include <sys/socket.h>
 
 #include <errno.h>
 #include <fcntl.h>
@@ -47,7 +48,7 @@
 #include "udev-utils.h"
 #include "utils.h"
 
-#define	DEVD_SOCK_PATH		"/var/run/devd.pipe"
+#define	DEVD_SOCK_PATH		"/var/run/devd.seqpacket.pipe"
 #define	DEVD_RECONNECT_INTERVAL	1000	/* reconnect after 1 second */
 
 #define	DEVD_EVENT_ATTACH	'+'
@@ -229,7 +230,7 @@ udev_monitor_thread(void *args)
 			continue;
 
 		if (fds[1].revents & POLLIN) {
-			if (socket_readline(devd_fd, ev, sizeof(ev)) < 0) {
+			if (recv(devd_fd, ev, sizeof(ev), MSG_WAITALL) < 0) {
 				close(devd_fd);
 				devd_fd = -1;
 				continue;

utils.c
@@ -63,7 +63,7 @@ socket_connect(const char *path)
 	struct sockaddr_un sa;
 	int fd;
 
-	fd = socket(AF_UNIX, SOCK_STREAM, 0);
+	fd = socket(AF_UNIX, SOCK_SEQPACKET, 0);
 	if (fd < 0)
                 return (-1);
 
@@ -76,23 +76,6 @@ socket_connect(const char *path)
 	}
 
 	return (fd);
-}
-
-ssize_t
-socket_readline(int fd, char *buf, size_t len)
-{
-	size_t pos;
-
-	for (pos = 0; pos < len; ++pos) {
-		if (read(fd, buf + pos, 1) < 1)
-			return (-1);
-
-		if (buf[pos] == 0 || buf[pos] == '\n') {
-			buf[pos] = 0;
-			return (pos);
-		}
-	}
-	return (-1);
 }
 
 /*

utils.h
@@ -66,7 +66,6 @@ char *strbase(const char *path);
 char *get_kern_prop_value(const char *buf, const char *prop, size_t *len);
 int match_kern_prop_value(const char *buf, const char *prop, const char *value);
 int socket_connect(const char *path);
-ssize_t socket_readline(int fd, char *buf, size_t len);
 int path_to_fd(const char *path);
 int scandir_recursive(char *path, size_t len, struct scan_ctx *ctx);
 #ifdef HAVE_DEVINFO_H

commit e0ff9741aab4060c6d5c36c504c3d791ad9571f4

Force NUL-termination of strings received from devd.
This fixes udev-monitor regression introduced in b2c96ce66a12

udev-monitor.c
@@ -193,6 +193,7 @@ udev_monitor_thread(void *args)
 	char ev[1024], syspath[DEV_PATH_MAX];
 	struct pollfd fds[2];
 	nfds_t nfds;
+	ssize_t len;
 	int devd_fd = -1, ret, action, timeout;
 	sigset_t set;
 	const static struct sockaddr_un sa = {
@@ -239,11 +240,14 @@ udev_monitor_thread(void *args)
 			continue;
 
 		if (fds[1].revents & POLLIN) {
-			if (recv(devd_fd, ev, sizeof(ev), MSG_WAITALL) < 0) {
+			if ((len = recv(devd_fd, ev, sizeof(ev), MSG_WAITALL))
+			    <= 0) {
 				close(devd_fd);
 				devd_fd = -1;
 				continue;
 			}
+			/* Replace terminating LF with 0 to make C-string */
+			ev[len - 1] = '\0';
 			action = parse_devd_message(ev, syspath, sizeof(syspath));
 			if (action != UD_ACTION_NONE &&
 			    udev_filter_match(um->udev, &um->filters, syspath))